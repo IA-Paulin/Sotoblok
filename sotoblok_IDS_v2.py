1eb24886ba8af9ee9847736e87cde31c37738442
IAPaulin <iapaulin.sibgu@gmail.com>
2021-08-06 10:35:38 +0700

    добавлено закрытие камеры при завершении скрипта

--- sotoblok_IDS_v2.py
+++ sotoblok_IDS_v2.py
@@ -24,6 +24,8 @@ frm = 0x20  # адрес от кого (адрес скрипта)
 to = 0x01  # адрес кому ( адрес сервера)
 line_cut_mass = []
 
+Camera = False #индикатор работы камеры
+
 
 # функция записывает данные в файл полученные при анализе контура
 def writer_data(file_name, mas):
@@ -44,196 +46,202 @@ def writer_data_alfa(file_name, mas):
 
 
 def main():
-    # init camera
-    hcam = ueye.HIDS(0)
-    ret = ueye.is_InitCamera(hcam, None)
-    print(f"initCamera returns {ret}")
 
-    # set color mode
-    m_nColorMode = ueye.IS_CM_BGR8_PACKED
-    ret = ueye.is_SetColorMode(hcam, ueye.IS_CM_BGR8_PACKED)
-    print(f"SetColorMode IS_CM_BGR8_PACKED returns {ret}")
+    try:
+        # init camera
+        hcam = ueye.HIDS(0)
+        ret = ueye.is_InitCamera(hcam, None)
+        print(f"initCamera returns {ret}")
 
-    # set region of interest
-    width = 1280
-    height = 1080
-    rect_aoi = ueye.IS_RECT()
-    rect_aoi.s32X = ueye.int(0)
-    rect_aoi.s32Y = ueye.int(0)
-    rect_aoi.s32Width = ueye.int(width)
-    rect_aoi.s32Height = ueye.int(height)
-    ueye.is_AOI(hcam, ueye.IS_AOI_IMAGE_SET_AOI, rect_aoi, ueye.sizeof(rect_aoi))
-    print(f"AOI IS_AOI_IMAGE_SET_AOI returns {ret}")
+        # set color mode
+        m_nColorMode = ueye.IS_CM_BGR8_PACKED
+        ret = ueye.is_SetColorMode(hcam, ueye.IS_CM_BGR8_PACKED)
+        print(f"SetColorMode IS_CM_BGR8_PACKED returns {ret}")
 
-    # allocate memory
-    mem_ptr = ueye.c_mem_p()
-    mem_id = ueye.int()
-    bitspixel = 24  # for colormode = IS_CM_BGR8_PACKED
-    ret = ueye.is_AllocImageMem(hcam, width, height, bitspixel,
-                                mem_ptr, mem_id)
-    print(f"AllocImageMem returns {ret}")
+        # set region of interest
+        width = 1280
+        height = 1080
+        rect_aoi = ueye.IS_RECT()
+        rect_aoi.s32X = ueye.int(0)
+        rect_aoi.s32Y = ueye.int(0)
+        rect_aoi.s32Width = ueye.int(width)
+        rect_aoi.s32Height = ueye.int(height)
+        ueye.is_AOI(hcam, ueye.IS_AOI_IMAGE_SET_AOI, rect_aoi, ueye.sizeof(rect_aoi))
+        print(f"AOI IS_AOI_IMAGE_SET_AOI returns {ret}")
 
-    # set active memory region
-    ret = ueye.is_SetImageMem(hcam, mem_ptr, mem_id)
-    print(f"SetImageMem returns {ret}")
+        # allocate memory
+        mem_ptr = ueye.c_mem_p()
+        mem_id = ueye.int()
+        bitspixel = 24  # for colormode = IS_CM_BGR8_PACKED
+        ret = ueye.is_AllocImageMem(hcam, width, height, bitspixel,
+                                    mem_ptr, mem_id)
+        print(f"AllocImageMem returns {ret}")
 
-    # continuous capture to memory
-    ret = ueye.is_CaptureVideo(hcam, ueye.IS_DONT_WAIT)
-    print(f"CaptureVideo returns {ret}")
+        # set active memory region
+        ret = ueye.is_SetImageMem(hcam, mem_ptr, mem_id)
+        print(f"SetImageMem returns {ret}")
 
-    # get data from camera and display
-    lineinc = width * int((bitspixel + 7) / 8)
+        # continuous capture to memory
+        ret = ueye.is_CaptureVideo(hcam, ueye.IS_DONT_WAIT)
+        print(f"CaptureVideo returns {ret}")
 
-    # setting fps ( для замедления камеры )
-    fps = ueye.c_double()
-    ueye.is_SetFrameRate(hcam, 3, fps)
-    print('fps = ', fps)
+        # get data from camera and display
+        lineinc = width * int((bitspixel + 7) / 8)
 
-    x = ueye.c_double(1)
-    y = ueye.c_double(0)
-    ueye.is_SetAutoParameter(hcam, ueye.IS_SET_ENABLE_AUTO_GAIN, x, y)
+        # setting fps ( для замедления камеры )
+        fps = ueye.c_double()
+        ueye.is_SetFrameRate(hcam, 3, fps)
+        print('fps = ', fps)
 
-    print('*******************************************')
-    print('width = ', width)
-    print('height = ', height)
-    print('bitspixel = ', bitspixel)
-    print('lineinc = ', lineinc)
-    print('m_nColorMode = ', m_nColorMode)
+        # делаем автоматическую подстройку яркости для камеры
+        x = ueye.c_double(1)
+        y = ueye.c_double(0)
+        ueye.is_SetAutoParameter(hcam, ueye.IS_SET_ENABLE_AUTO_GAIN, x, y)
 
-    om = 1
-    om_big = 1
-    points = []
-    sota = 60
+        print('*******************************************')
+        print('width = ', width)
+        print('height = ', height)
+        print('bitspixel = ', bitspixel)
+        print('lineinc = ', lineinc)
+        print('m_nColorMode = ', m_nColorMode)
 
-    print('СТАРТ ПРОГРАММЫ')
+        om = 1
+        om_big = 1
+        points = []
+        sota = 60
 
-    while True:
-        global line_cut
-        img = ueye.get_data(mem_ptr, width, height, bitspixel, lineinc, copy=True)
-        img = np.reshape(img, (height, width, 3))
+        print('СТАРТ ПРОГРАММЫ')
 
-        # img = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
+        while True:
+            global line_cut
+            img = ueye.get_data(mem_ptr, width, height, bitspixel, lineinc, copy=True)
+            img = np.reshape(img, (height, width, 3))
 
-        img_sniper = img.copy()
+            # img = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
 
-        h, w, _ = img.shape
+            img_sniper = img.copy()
 
-        cv.line(img_sniper, (0, int(h / 2)), (w, int(h / 2)), (0, 0, 255), 1)
-        cv.line(img_sniper, (int(w / 2), 0), (int(w / 2), h), (0, 0, 255), 1)
+            h, w, _ = img.shape
 
-        cv.imshow('IP Camera stream', img_sniper)
-        cv.waitKey(1)
+            cv.line(img_sniper, (0, int(h / 2)), (w, int(h / 2)), (0, 0, 255), 1)
+            cv.line(img_sniper, (int(w / 2), 0), (int(w / 2), h), (0, 0, 255), 1)
 
-        # Производим калибровку для общего снимка (поиск точек контура)
-        if e_cal_big.wait(timeout=0):
-            print('Калибровка большого снимка')
-            file_name = './calibrovka_big.jpg'
-            cv.imwrite(file_name, img)
+            cv.imshow('IP Camera stream', img_sniper)
+            cv.waitKey(1)
 
-            om_big, al_big = calibration.calibration(file_name)  # проводим калибровку
-            om_big = om_big * 1.04575  # вводим коэфициент для точности калибровки
-            print('one_mm = ', om_big)
-            print('angle = ', al_big)
-            e_cal_big.clear()
+            # Производим калибровку для общего снимка (поиск точек контура)
+            if e_cal_big.wait(timeout=0):
+                print('Калибровка большого снимка')
+                file_name = './calibrovka_big.jpg'
+                cv.imwrite(file_name, img)
 
-            # Передаем количество пикселей в миллиметре и угол на сервер
-            data = struct.pack('<ff', om_big, al_big)
-            wake.tx(frm, to, 0x21, data)
-            sleep(0.1)
+                om_big, al_big = calibration.calibration(file_name)  # проводим калибровку
+                om_big = om_big * 1.04575  # вводим коэфициент для точности калибровки
+                print('one_mm = ', om_big)
+                print('angle = ', al_big)
+                e_cal_big.clear()
 
-        # Производим калибровку для снимка с приближением (поиск точек реза)
-        if e_cal_small.wait(timeout=0):
-            print('Калибровка малого снимка')
-            file_name = './calibrovka_small.jpg'
-            cv.imwrite(file_name, img)
-
-            om, al = calibration.calibration(file_name)  # проводим калибровку
-            om = om * 1.04575  # вводим коэфициент для точности калибровки
-            print('one_mm = ', om)
-            print('angle = ', al)
-            e_cal_small.clear()
-
-            # Передаем количество пикселей в миллиметре и угол на сервер
-            data = struct.pack('<ff', om, al)
-            wake.tx(frm, to, 0x22, data)
-            sleep(0.1)
-
-        # Производим определения контура заготовки сотоблока
-        if e_fe.wait(timeout=0):
-            print('Контур сотоблока')
-            file_name = './contur_sotoblok.jpg'
-            file_name = os.path.abspath(file_name)
-            cv.imwrite(file_name, img)
-
-            start = 10  # размер матрицы от центра т.е цент +start в право и ввер  и -start влево и вниц, при start =10 матрица 20х20
-            step = 6  # шаг сканирования матрицы
-
-            print('om_big =', om_big)
-            print(file_name)
-            # TODO не забудь удалить это введено для тестов
-            om_big = 13
-            # ****************************
-            koor = fe.find_conturs_cotoblok(file_name, start, step, om_big)  # определяем контур сотоблока
-            e_fe.clear()
-
-            if koor == -1:
-                data = 1
-                wake.tx(frm, to, 0x23, data)
-            else:
-                writer_data('./log/koor.txt', koor)
-                # Передаем  массив точек контура объекта
-                for i in range(len(koor)):
-                    data = struct.pack('<ff', koor[i][0], koor[i][1])
-                    wake.tx(frm, to, 0x23, data)
+                # Передаем количество пикселей в миллиметре и угол на сервер
+                data = struct.pack('<ff', om_big, al_big)
+                wake.tx(frm, to, 0x21, data)
                 sleep(0.1)
 
-        # Производим определения точек реза сотоблока
-        if e_fp.wait(timeout=0):
-            print('Поиск узлов')
-            file_name = './start.jpg'
-            cv.imwrite(file_name, img)
+            # Производим калибровку для снимка с приближением (поиск точек реза)
+            if e_cal_small.wait(timeout=0):
+                print('Калибровка малого снимка')
+                file_name = './calibrovka_small.jpg'
+                cv.imwrite(file_name, img)
 
-            s = binarizacia.binariz(file_name)  # проводим бинаризацию, возвращает путь обработанного файла
+                om, al = calibration.calibration(file_name)  # проводим калибровку
+                om = om * 1.04575  # вводим коэфициент для точности калибровки
+                print('one_mm = ', om)
+                print('angle = ', al)
+                e_cal_small.clear()
 
-            if s == -1:  # необходимо проверять яркость изображения, если изображение засвечено, дальнейшая обработка не возможна
-                print('Image brightness')
-                e_fp.clear()
-            else:
-                print('om =', om)
-                print('line_cut_mass in prer = ', line_cut_mass)
-                points, alfa = fp.find_point(s, om, line_cut_mass)
-                writer_data('./log/points_cut.txt', points)
-                writer_data_alfa('./log/alfa.txt', alfa)
-                e_fp.clear()
+                # Передаем количество пикселей в миллиметре и угол на сервер
+                data = struct.pack('<ff', om, al)
+                wake.tx(frm, to, 0x22, data)
+                sleep(0.1)
 
-                # передаем координаты реза на сервер
-                if len(points) == len(alfa):
-                    for i in range(len(points)):
-                        data = struct.pack('<fff', points[i][0], points[i][1], alfa[i])
-                        wake.tx(frm, to, 0x24, data)
-                else:
+            # Производим определения контура заготовки сотоблока
+            if e_fe.wait(timeout=0):
+                print('Контур сотоблока')
+                file_name = './contur_sotoblok.jpg'
+                file_name = os.path.abspath(file_name)
+                cv.imwrite(file_name, img)
+
+                start = 10  # размер матрицы от центра т.е цент +start в право и ввер  и -start влево и вниц, при start =10 матрица 20х20
+                step = 6  # шаг сканирования матрицы
+
+                print('om_big =', om_big)
+                print(file_name)
+                # TODO не забудь удалить это введено для тестов
+                om_big = 13
+                # ****************************
+                koor = fe.find_conturs_cotoblok(file_name, start, step, om_big)  # определяем контур сотоблока
+                e_fe.clear()
+
+                if koor == -1:
                     data = 1
-                    wake.tx(frm, to, 0x24, data)
+                    wake.tx(frm, to, 0x23, data)
+                else:
+                    writer_data('./log/koor.txt', koor)
+                    # Передаем  массив точек контура объекта
+                    for i in range(len(koor)):
+                        data = struct.pack('<ff', koor[i][0], koor[i][1])
+                        wake.tx(frm, to, 0x23, data)
+                    sleep(0.1)
 
-                sleep(0.1)
+            # Производим определения точек реза сотоблока
+            if e_fp.wait(timeout=0):
+                print('Поиск узлов')
+                file_name = './start.jpg'
+                cv.imwrite(file_name, img)
 
-        # Завершаем работу камеры
-        if e_exit.wait(timeout=0):
-            data = struct.pack('<H', 0)
-            wake.tx(frm, to, 0x26, data)
+                s = binarizacia.binariz(file_name)  # проводим бинаризацию, возвращает путь обработанного файла
 
-            print('Завершение работы камеры')
-            break
+                if s == -1:  # необходимо проверять яркость изображения, если изображение засвечено, дальнейшая обработка не возможна
+                    print('Image brightness')
+                    e_fp.clear()
+                else:
+                    print('om =', om)
+                    print('line_cut_mass in prer = ', line_cut_mass)
+                    points, alfa = fp.find_point(s, om, line_cut_mass)
+                    writer_data('./log/points_cut.txt', points)
+                    writer_data_alfa('./log/alfa.txt', alfa)
+                    e_fp.clear()
 
-    e_exit.clear()
-    print('END PROGRAMM')
-    cv.destroyAllWindows()
+                    # передаем координаты реза на сервер
+                    if len(points) == len(alfa):
+                        for i in range(len(points)):
+                            data = struct.pack('<fff', points[i][0], points[i][1], alfa[i])
+                            wake.tx(frm, to, 0x24, data)
+                    else:
+                        data = 1
+                        wake.tx(frm, to, 0x24, data)
 
-    # cleanup
-    ret = ueye.is_StopLiveVideo(hcam, ueye.IS_FORCE_VIDEO_STOP)
-    print(f"StopLiveVideo returns {ret}")
-    ret = ueye.is_ExitCamera(hcam)
-    print(f"ExitCamera returns {ret}")
+                    sleep(0.1)
+
+            # Завершаем работу камеры
+            if e_exit.wait(timeout=0):
+                data = struct.pack('<H', 0)
+                wake.tx(frm, to, 0x26, data)
+
+                print('Завершение работы камеры')
+                break
+
+        e_exit.clear()
+        print('END PROGRAMM')
+        cv.destroyAllWindows()
+
+        # cleanup
+        ret = ueye.is_StopLiveVideo(hcam, ueye.IS_FORCE_VIDEO_STOP)
+        print(f"StopLiveVideo returns {ret}")
+        ret = ueye.is_ExitCamera(hcam)
+        print(f"ExitCamera returns {ret}")
+
+    except Exception:
+        print("ERROR")
 
 
 def segment(data):
@@ -316,6 +324,7 @@ def points(data):
 
 def rx(frm, to, cmd, data):
     global wake
+    global Camera
 
     print('rx')
     if to == 0 or to == addr:
@@ -330,10 +339,17 @@ def rx(frm, to, cmd, data):
             points(data)
             e_fp.set()  # поиск точек реза
         elif cmd == 0x25:
+            Camera = True
             e_power_cam.set()  # запуск камеры
         elif cmd == 0x26:
+            Camera = False
             e_exit.set()  # завершение работы камеры
         elif cmd == 0x27:
+
+            if Camera:       #сперва завершаем работу камеры
+                e_exit.set()
+                sleep(1)
+
             e_end_script.set()  # завершение программы
             wake.stop()
         else:
